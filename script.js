359432ab2878a93fd02bb06d8fe08dda35b8eb5e
smckenzie78 <109680885+smckenzie78@users.noreply.github.com>
2023-01-22 00:46:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,75 @@
-let screen = document.getElementById('calcscreen')
-let operand1 = 0;
+let screen = document.getElementById('calcscreen');
+screen.value = '';
+let firstInput = true;
 let result = 0;
-let operand2=0;
 
 function acclearscreen(){
-    screen.value = '';
-}
-
-function opclearscreen(){
+    screen.style.color = "white";
     screen.value = '';
 }
 
 function input(value1){
-        screen.value = screen.value + value1;
-        //console.log(value1);
-    
+        if(firstInput==true || screen.value==''){
+            acclearscreen();
+            firstInput = false;
+        }
+        if(!isNaN(value1)){
+            screen.value = screen.value + value1;
+        }
 }
 
 
 function addition(){
-    screen.value = screen.value + ' + ';
+    screen.style.color = "white";
+    if(screen.value == ''){
+        screen.value =  '0 + ';
+        firstInput = false;
+    }
+    else{
+        screen.value = screen.value + ' + ';
+    }
 }
 function subtraction(){
-    screen.value = screen.value + ' - ';
+    screen.style.color = "white";
+    if(screen.value == ''){
+        screen.value =  '0 - ';
+        firstInput = false;
+    }
+    else{
+        screen.value = screen.value + ' - ';
+    }
 }
 function multiplication(){
-    screen.value = screen.value + ' * ';
+    screen.style.color = "white";
+    if(screen.value == ''){
+        screen.value =  '0 * ';
+        firstInput = false;
+    }
+    else{
+        screen.value = screen.value + ' * ';
+    }
 }
 function division(){
-    screen.value = screen.value + ' / ';
+    screen.style.color = "white";
+    if(screen.value == ''){
+        screen.value =  '0 / ';
+        firstInput = false;
+    }
+    else{
+        screen.value = screen.value + ' / ';
+    }
 }
 function negative(){
+    screen.style.color = "white";
     screen.value = screen.value + ' * (-1) ';
 }
 function percentage(){
     result = eval(screen.value) / 100;
     screen.value = result;
+    screen.style.color = "cyan";
 }
 function screenequals(){
     result = eval(screen.value);
     screen.value = result;
+    screen.style.color = "cyan";
 }
